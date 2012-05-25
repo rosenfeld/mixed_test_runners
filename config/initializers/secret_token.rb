0ea26b9913d233bf8c8991456f5a053a098e826a
Rodrigo Rosenfeld Rosas <rr.rosas@gmail.com>
2012-05-25 00:38:46 -0300

    rails new mixed_specs -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MixedSpecs::Application.config.secret_token = '71e9c4915fcb91c2e6ffb7c436859ebe2adab760b1bad4aa6ade80d3fb2daa81f10ef3c785503eee72be2f59fac58705bf2172a47a082dfd8e0ca57ceb1cc925'
